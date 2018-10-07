738436e20f51f86eff15c5ff66064f35ac6f6ec9
motttey <motitago@gmail.com>
2018-10-07 17:13:34 +0900

    get estate name from town name

--- scrape.py
+++ scrape.py
@@ -1,4 +1,6 @@
 import urllib2
+from urlparse import urlparse
+import requests
 from bs4 import BeautifulSoup
 
 def scrape_towns():
@@ -18,12 +20,24 @@ def scrape_towns():
     return popular_town_array
 
 def get_real_estate_list(town_name_list):
+    all_estate_name_list = {}
     for town_name in town_name_list:
-        print("https://suumo.jp/library/search/ichiran.html?qr="+town_name.encode('utf-8')+"&qk=3")
-        html = urllib2.urlopen("https://suumo.jp/library/search/ichiran.html?qr="+town_name.encode('utf-8')+"&qk=3")
-        soup = BeautifulSoup(html, features="html.parser")
+        print("https://www.athome.co.jp/bldg-library/bldname_search/"+town_name.encode('utf-8')+"/")
+        print("https://www.sumai-surfin.com/search/result/?prefecture_id=&keyword="+urllib2.quote(town_name.encode('utf-8')))
+        response = urllib2.urlopen("https://www.sumai-surfin.com/search/result/?prefecture_id=&keyword="+urllib2.quote(town_name.encode('utf-8')))
+        # req.add_header('User-Agent', User_Agent)
+        soup = BeautifulSoup(response, features="html.parser")
+
+        estate_name_list = []
+        estate_list = soup.find_all('div', {'class' :'p-section__ttl-line'})
+        for estate in estate_list:
+            estate_name = estate.find('div').find('a').text
+            estate_name_list.append(estate_name.encode('utf-8'))
+        all_estate_name_list[town_name.encode('utf-8')] = estate_name_list
+    return all_estate_name_list
 
 if __name__ == '__main__':
     popular_town_array = scrape_towns()
     print(popular_town_array)
-    get_real_estate_list(popular_town_array)
+    all_estate_name_list = get_real_estate_list(popular_town_array)
+    print(all_estate_name_list["横浜"])
